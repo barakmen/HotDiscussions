97955de04052573863b9e1ddd4c0b52d185a46a8
barak menachem <barakmen@post.bgu.ac.il>
2018-05-14 14:51:59 +0300

    startup files

--- startup_files/HDPstartup.bat
+++ startup_files/HDPstartup.bat
@@ -1,24 +1,25 @@
 date /t
-@echo on
-echo echo turned on
-date /t
+set currentFolder="startup_files"
 
-REM booting our HDP database
-call npm install
-REM REM  start mongod --config C:\Users\srominm\Documents\Projects\HotDiscussions\DB\mongodb.conf
+set nodeModules="node_modules"
+if not exist %nodeModules% REM installing npm...
+if not exist %nodeModules% CMD /C npm install
+
+
+REM booting our HDP database...
+set mongodApp=C:/bin/mongod
+start %mongodApp% --config mongodb.conf
 
 timeout 5
 
 REM pm2 is an npm package that restarts after crashing - restarts node in case of crashing to keep the server live
+set pm2App=node_modules/.bin/pm2
 
-set pm2App=../node_modules/.bin/pm2
-
-REM boot starts our HDP server
-REM set serverBootApp=C:\Users\srominm\Documents\GitHub\HotDiscussionsV2/boot.js
-
-REM as of end of October we are going to use a cluster of apps running, each for clients and a single app for testing environment
-REM each app will be running on its unique port. Testing:3000, Benzi:3001, Baruch:3002, Tsafrir:3003.
-REM TEMP!! each app will be running on its unique port. Testing:3003, Benzi:3001, Baruch:3002, Tsafrir:3001.
+:: as of end of October we are going to use a cluster of apps running, each for clients and a single app for testing environment
+:: each app will be running on its unique port. Testing:3000, Benzi:3001, Baruch:3002, Tsafrir:3003.
+:: TEMP!! each app will be running on its unique port. Testing:3003, Benzi:3001, Baruch:3002, Tsafrir:3001.
 
 REM starting node app on 4 different instances 
-REM REM %pm2App% start process.json
+%pm2App% start %currentFolder%/process.json
+::%pm2App% delete all
+
