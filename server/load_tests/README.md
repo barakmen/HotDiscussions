1d145ae7cbb69cacefd370a4728fb7f6589b78d3
Whats now <barakmen@outlook.com>
2018-05-27 17:04:56 +0300

    register users

--- server/load_tests/README.md
+++ server/load_tests/README.md
@@ -6,7 +6,7 @@ Or simply do the following steps:
 1. Download k6 from [here](https://github.com/loadimpact/k6/releases).
 2. Extract the downloaded zip and use the terminal to `cd` into the extracted folder.
 3. copy the file: `<cloned-repository-folder>/server/load_tests/firsttest.js`
-3. from the extracted folder run `k6 run <cloned-repository-folder>/server/load_tests/firsttest.js`
+3. from the extracted folder run `./k6 run <cloned-repository-folder>/server/load_tests/firsttest.js`
 
 
 Install influxdb and go on Windows: [here](https://stackoverflow.com/questions/26116711/how-to-install-influxdb-in-windows)
--- server/load_tests/firsttest.js
+++ server/load_tests/firsttest.js
@@ -8,22 +8,43 @@ const localhost = "http://localhost:" + portForLoadtests;
 var serverUrl = localhost;
 
 export let options = {
-  vus: 1,
+  vus: 100,
   duration: "10s"
 };
 export default function() {
-    getMainPage();
-    register();
+    getMainPage_test();
+    registerUsers_test();
 };
 
-function register(){
+
+
+function registerUsers_test(){
+    let res = http.get('http://randomuser.me/api/',  { json: true });
+    var userInfo = JSON.parse(res.body).results[0];
+      var data = {
+        username : userInfo.login.username,
+        password : userInfo.login.password,
+        lname : userInfo.name.first,
+        fname : userInfo.name.last,
+        email : userInfo.email
+      }
+      console.log(JSON.stringify(data));
+      register(data);
+
+}
+//data is from format : {username:'a', password:'a', fname:'fb', lname:'lb', email:'a@a.com'}
+function register(data){
     let res = http.get(serverUrl + "/auth/register");
     check(res, {"success": (r) => r.status == 200});
 
-
+    let res1 = http.post(serverUrl + "/auth/register", data);
+        check(res1, {"success": (r) =>  {
+                return r.status == 200 && r.url === '/auth/login';
+            }
+        });
 }
 
-function getMainPage(){
+function getMainPage_test(){
     let res = http.get(serverUrl);
     check(res, {"success": (r) => r.status == 200});
 }
