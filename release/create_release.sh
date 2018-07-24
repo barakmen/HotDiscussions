04c0f140f7cc3584a511a69ab5f4474620884a8b
barakmen <barakmen@outlook.com>
2018-07-24 14:03:16 +0300

    'auto bump it to version 2.1'

--- release/create_release.sh
+++ release/create_release.sh
@@ -1,7 +1,7 @@
 #first argument is the release num
 git checkout develop
 git add .
-git commit -m "auto bump it to version $1"
+git commit -m "'auto bump it to version $1'"
 git checkout -b release-$1 develop
 sh release/bump_version.sh $1
 git checkout master
